db02ea3eedcd8bf041d7a33a1ce5e1656806cd1a
ShilpiSaha97 <50799273+ShilpiSaha97@users.noreply.github.com>
2022-05-25 20:25:59 +0530

    Create Solution3.java

---
+++ Solution3.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution3 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.close();
+        
+        if(N%2==1){
+            System.out.println("Weird");
+        }
+        if(N%2==0 && N>=2 && N<=5){
+            System.out.println("Not Weird");
+        }
+        if(N%2==0 && N>=6 && N<=20){
+            System.out.println("Weird");
+        }
+        if(N>20 && N%2==0){
+            System.out.println("Not Weird");
+        }
+    }
+}
