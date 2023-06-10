63741906dac92296d777ea6ccd3445f3c08ee128
ShilpiSaha97 <50799273+ShilpiSaha97@users.noreply.github.com>
2023-06-10 18:06:49 +0530

    Update WeightedGraph.java

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -42,7 +42,7 @@ public class WeightedGraph {
         gr[4].add(new Edge(4,5,5));
     }
 
-    //shortest path
+    //shortest path using Dijkstra Algo(only valid for +ve weights)
     public static void DijkstraAlgo(ArrayList<Edge> gr[], int src, int v){
 
         PriorityQueue<Pair> pq=new PriorityQueue<>();
@@ -77,12 +77,37 @@ public class WeightedGraph {
             System.out.println(dist[i]);
         }
     }
+    
+    //BellmanFord algo for shortest path-->valid for both +ve and -ve weights
+public static void bellmanFord(ArrayList<Edge> graph[], int src) {
+int dist[] = new int[graph.length];
+for(int i=0; i<dist.length; i++) {
+if(i != src)
+dist[i] = Integer.MAX_VALUE;
+}
+//O(V)
+for(int i=0; i<graph.length-1; i++) {
+//edges - O(E)
+for(int j=0; j<graph.length; j++) {
+for(int k=0; k<graph[j].size(); k++) {
+Edge e = graph[j].get(k);
+int u = e.src;
+int v = e.dest;
+int wt = e.wt;
+if(dist[u] != Integer.MAX_VALUE && dist[u]+wt < dist[v]) {
+dist[v] = dist[u] + wt;
+}
+}
+}
+}
 
     public static void main(String[] args){
         int v=6;
         ArrayList<Edge> gr[]=new ArrayList[v];
         create(gr);
         DijkstraAlgo(gr,0,v);
+        
+        
 
     }
 }
